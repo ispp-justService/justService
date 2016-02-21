df661cebd9e53f4e85c5a27c4cfdf4df35f15d29
ispp <domgaromero@gmail.com>
2016-02-21 21:38:42 +0100

    Primera carga del proyecto

---
+++ application/views/footer.php
@@ -0,0 +1,8 @@
+
+
+	<script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-1.10.2.js"); ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+
+<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+</body>
+</html>
---
+++ application/views/main/justService.php
@@ -0,0 +1,8 @@
+<h1>Just Service</h1>
+
+<div class="alert alert-danger">
+</div>
+
+<div class="panel panel-primary">
+	<div class="panel-body">Ejemplo</div>
+</div>
