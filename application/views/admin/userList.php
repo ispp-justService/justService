31f3fd0c5aaa1fc29706f2d16a4134035e8e4409
David <davferrod3@alum.us.es>
2016-03-30 13:44:28 +0200

    Añadida la vista User's list
    La vista user's list contiene paneles con la información de cada usuario
    que se ha registrado en la aplicación.

---
+++ application/views/admin/userList.php
@@ -0,0 +1,38 @@
+    <div class="container">
+      <div class="row row-centered">
+	<h3>User's list</h3>
+	<hr>
+	<div class="col-xs-6 col-sm-6 col-md-6 col-centered">
+		<div class="panel panel-default" >
+		   <div class="panel-heading">Nombre Apellidos</div>
+		      <div class="panel-body">
+			<div class="row">
+			   <div class="col-md-8">
+			      	<table>
+				   <tr>
+					<td><b>Email:</b> prueba@gmail.com</td>	
+				   </tr>
+				   <tr>
+					<td><b>Moment:</b> 30/03/2016 13:19</td>	
+				   </tr>
+				   <tr>
+					<td><b>Discount available:</b> 20%</td>	
+				   </tr>
+				   <tr>
+					<td><b>Zip Code:</b> 41412</td>	
+				   </tr>
+				   <tr>
+					<td><b>Phone Number:</b> 654789987</td>	
+				   </tr>				
+				</table>
+			    </div>
+			    <div class="col-md-4">
+				<img src="<?php echo base_url("assets/img/avatar-logo.png"); ?>" id="logo" wight ="100px" height="100px"/>
+			    </div>
+			</row>
+		      </div>
+		   </div>
+		</div>
+	</div> <!-- /class cols  -->
+      </div> <!-- /row -->
+    </div> <!-- /container -->
