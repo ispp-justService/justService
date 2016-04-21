bd98a01c4a29e334d787e5d518fcd6966c64dc73
ispp <domgaromero@gmail.com>
2016-04-21 13:16:07 +0200

    Aspecto visual de la paginación

--- application/controllers/Service.php
+++ application/controllers/Service.php
@@ -39,6 +39,26 @@ class Service extends CI_Controller {
 		// Reusamos la misma cadena de búsqueda que hemos usado anteriormente
 		$config['reuse_query_string'] = TRUE;
 
+		// Configuración visual de la paginación con Bootstrap
+        $config['full_tag_open'] = '<ul class="pagination">';
+        $config['full_tag_close'] = '</ul>';
+        $config['first_link'] = false;
+        $config['last_link'] = false;
+        $config['first_tag_open'] = '<li>';
+        $config['first_tag_close'] = '</li>';
+        $config['prev_link'] = '&laquo';
+        $config['prev_tag_open'] = '<li class="prev">';
+        $config['prev_tag_close'] = '</li>';
+        $config['next_link'] = '&raquo';
+        $config['next_tag_open'] = '<li>';
+        $config['next_tag_close'] = '</li>';
+        $config['last_tag_open'] = '<li>';
+        $config['last_tag_close'] = '</li>';
+        $config['cur_tag_open'] = '<li class="active"><a href="#">';
+        $config['cur_tag_close'] = '</a></li>';
+        $config['num_tag_open'] = '<li>';
+        $config['num_tag_close'] = '</li>';
+
 		// Iniciamos la paginación
 		$this->pagination->initialize($config);
 
