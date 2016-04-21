e5b8cbc112edbe0ffd622c839365f5786e9e8c4a
David <davferrod3@alum.us.es>
2016-04-21 17:32:33 +0200

    Corregido el estilo del buscador principal

--- application/views/main/main.php
+++ application/views/main/main.php
@@ -5,19 +5,19 @@
       </div>
 
     <div class="container">
-      <!-- Example row of columns -->
       <div class="row">
         <div class="container text-center" id="search">
+	<form action="<?php echo site_url('/service/search') ?>" method="GET">
 	<div class="input-group borderCentral">
-		<form action="<?php echo site_url('/service/search') ?>" method="GET">
-	     <input type="text" name="text_search" class="form-control searchCentral" id="search-input" placeholder="What do you need?">
-	     <span class="input-group-btn">		
-		<button class="btn btn-default" type="submit" id="search-span" >
-		    <span class="glyphicon glyphicon-search" ><span/>
-		</button>
-	     </span>
-		</form>
+		
+	     	     <input type="text" name="text_search" class="form-control searchCentral" id="search-input" placeholder="What do you need?">
+		     <span class="input-group-btn">		
+			<button class="btn btn-default" type="submit" id="search-span" >
+			    <span class="glyphicon glyphicon-search" ><span/>
+			</button>
+		     </span>
 	</div>
+	</form>
         </div>
       </div>
 
