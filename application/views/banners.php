b9c5708314905040bf0b1cb442ed3c8346f1ea45
David <davferrod3@alum.us.es>
2016-04-27 13:38:12 +0200

    Mejora de la maquetación de los banners del footer

--- application/views/banners.php
+++ application/views/banners.php
@@ -3,14 +3,14 @@
 
 <div class="container">
       <div class="row">
-        <div class="col-md-4">
-          <a href="#"><img src="<?php echo base_url("assets/img/banner.png"); ?>" class="img-thumbnail" /></a>
+        <div class="col-md-4 col-xs-4 col-lg-4 col-sm-4">
+          <a href="#"><img src="<?php echo base_url("assets/img/banner.png"); ?>" class="img-thumbnail img-responsive" /></a>
         </div>
-        <div class="col-md-4">
-          <a href="#"><img src="<?php echo base_url("assets/img/banner.png"); ?>" class="img-thumbnail" /></a>
+        <div class="col-md-4 col-xs-4 col-lg-4 col-sm-4">
+          <a href="#"><img src="<?php echo base_url("assets/img/banner.png"); ?>" class="img-thumbnail img-responsive" /></a>
        </div>
-        <div class="col-md-4">
-          <a href="#"><img src="<?php echo base_url("assets/img/banner.png"); ?>" class="img-thumbnail" /></a>
+        <div class="col-md-4 col-xs-4 col-lg-4 col-sm-4">
+          <a href="#"><img src="<?php echo base_url("assets/img/banner.png"); ?>" class="img-thumbnail img-responsive" /></a>
         </div>
       </div>
 </div>
